2e45cc687bca1325424787f37ad1608f69dfe424
Sneha Patil <sneha.patil13sp@gmail.com>
2023-01-11 16:30:46 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (python3)

---
+++ leetcode/problems/intersection_of_two_arrays_ii/solution.py
@@ -0,0 +1,12 @@
+import collections
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        n1 = collections.Counter(nums1)
+        res = []
+
+        for num in nums2:
+            if n1[num] > 0:
+                res.append(num)
+                n1[num] -= 1
+        
+        return res
