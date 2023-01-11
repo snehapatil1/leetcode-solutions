ccd7c591fe20515fe795172a6e64ef4d01c422b8
Sneha Patil <sneha.patil13sp@gmail.com>
2023-01-11 21:18:58 +0530

    Update 1_Two_Sum.py

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,3 +1,9 @@
+"""
+    Data Structures: Array, Hash Table
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+"""
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         index_dict = {}
