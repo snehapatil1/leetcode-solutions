c3d9f7a72a3093fc050cad27971e7ad61ab81b53
Sneha Patil <sneha.patil13sp@gmail.com>
2023-01-14 22:27:32 +0530

    Create 566_Reshape_the_Matrix.py

---
+++ 566_Reshape_the_Matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if not mat:
+            return mat
+        
+        if (len(mat) * len(mat[0])) != r * c:
+            return mat
+        
+        new_matrix = []
+        temp = []
+
+        for x in mat:
+            for y in x:
+                temp.append(y)
+
+        current_index = 0
+        for row in range(r):
+            new_matrix.append(temp[current_index:current_index+c])
+            current_index = current_index+c
+
+        return new_matrix
