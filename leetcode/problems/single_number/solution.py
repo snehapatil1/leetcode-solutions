4ff94232aa59056170a44ba896e407a6e4a111ca
Sneha Patil <sneha.patil13sp@gmail.com>
2023-01-17 14:44:16 +0000

    Sync LeetCode submission - Single Number (python3)

---
+++ leetcode/problems/single_number/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_map = {}
+        for i in nums:
+            if i not in hash_map.keys():
+                hash_map.update({i: 1})
+            else:
+                hash_map[i] += 1
+        
+        for i in hash_map:
+            if hash_map[i] == 1:
+                return i
