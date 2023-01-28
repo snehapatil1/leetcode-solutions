5b8920c5dc1d2224183333cafc4e5c5a42b7e876
Sneha Patil <sneha.patil13sp@gmail.com>
2023-01-28 14:07:27 +0000

    Sync LeetCode submission - Reverse String (python3)

--- leetcode/problems/reverse_string/solution.py
+++ leetcode/problems/reverse_string/solution.py
@@ -3,16 +3,4 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        if not len(s)>0:
-            return s
-        def recursion(start, end, s):
-            if (start == end) or (start < 0) or (end > len(s)) or (start > end):
-                return
-            temp = s[start]
-            s[start] = s[end]
-            s[end] = temp
-            start += 1
-            end -=1
-            recursion(start, end, s)
-        
-        recursion(0, len(s)-1, s)
+        return s.reverse()
