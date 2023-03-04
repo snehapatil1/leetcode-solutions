8f9c7ef7cf0071debdb5547eb2005eeeccb99634
Sneha Patil <sneha.patil13sp@gmail.com>
2023-03-04 21:05:33 +0530

    Create 49_Group_Anagrams.py

---
+++ Array/49_Group_Anagrams.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams_map = {}
+
+        for word in strs:
+            sorted_string = ''.join(sorted(word))
+            if sorted_string not in anagrams_map.keys():
+                anagrams_map[sorted_string] = []
+            anagrams_map[sorted_string].append(word)
+        
+        output = []
+        for key in anagrams_map:
+            output.append(anagrams_map[key])
+        
+        return output
