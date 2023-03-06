1a8dbe6e2bcf1578c55384459fbacc772242e905
Sneha Patil <sneha.patil13sp@gmail.com>
2023-03-06 13:39:52 +0000

    Sync LeetCode submission - Permutations (python3)

---
+++ leetcode/problems/permutations/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        def dfs(nums, path, res):
+            if not nums:
+                res.append(path)
+            for i in range(len(nums)):
+                dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+        
+        res = []
+        dfs(nums, [], res)
+        return res
