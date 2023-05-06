ae5ae9c143739294fc6e6a3ea3441c0fb62f1e76
Sneha Patil <sneha.patil13sp@gmail.com>
2023-05-06 23:00:02 +0530

    Update 324. Wiggle Sort II.py

--- Array/324. Wiggle Sort II.py
+++ Array/324. Wiggle Sort II.py
@@ -1,3 +1,9 @@
+"""
+    Question:
+        Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+        You may assume the input array always has a valid answer.
+"""
+
 class Solution:
     def wiggleSort(self, nums: List[int]) -> None:
         """
