c4b638585af513c9a61901d7d60b28b53d6f5a9c
Sneha Patil <sneha.patil13sp@gmail.com>
2023-05-07 18:37:25 +0530

    Update 334. Increasing Triplet Subsequence.py

--- Array/334. Increasing Triplet Subsequence.py
+++ Array/334. Increasing Triplet Subsequence.py
@@ -1,3 +1,10 @@
+"""
+    Question:
+        Given an integer array nums, return true if there exists a triple of indices (i, j, k) 
+        such that i < j < k and nums[i] < nums[j] < nums[k].
+        If no such indices exists, return false.
+"""
+
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         first_number = float(inf)
