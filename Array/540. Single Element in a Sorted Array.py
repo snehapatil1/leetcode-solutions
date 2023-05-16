ea517c68a4a2defedfbd3799de3ee4835303dbec
Sneha Patil <sneha.patil13sp@gmail.com>
2023-05-16 12:13:45 +0530

    Update 540. Single Element in a Sorted Array.py

--- Array/540. Single Element in a Sorted Array.py
+++ Array/540. Single Element in a Sorted Array.py
@@ -1,3 +1,11 @@
+"""
+    Question:
+        You are given a sorted array consisting of only integers where every element appears exactly twice, 
+        except for one element which appears exactly once.
+        Return the single element that appears only once.
+        Your solution must run in O(log n) time and O(1) space.
+"""
+
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
