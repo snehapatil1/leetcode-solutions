1ede88c0ad5d1b1397ca8820256d1866ecfa56d4
Sneha Patil <sneha.patil13sp@gmail.com>
2023-05-19 09:58:58 +0530

    Update 560. Subarray Sum Equals K.py

--- Array/560. Subarray Sum Equals K.py
+++ Array/560. Subarray Sum Equals K.py
@@ -1,3 +1,9 @@
+"""
+    Question:
+        Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+        A subarray is a contiguous non-empty sequence of elements within an array.
+"""
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         count = 0
