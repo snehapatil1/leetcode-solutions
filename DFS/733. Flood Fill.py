221b925251bd69d3f4eb2899b146bcfb40a08d60
Sneha Patil <sneha.patil13sp@gmail.com>
2023-08-05 15:38:04 +0530

    Update 733. Flood Fill.py

--- DFS/733. Flood Fill.py
+++ DFS/733. Flood Fill.py
@@ -1,3 +1,14 @@
+"""
+    Question:
+        An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+        You are also given three integers sr, sc, and color. 
+        You should perform a flood fill on the image starting from the pixel image[sr][sc].
+        To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the 
+        same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. 
+        Replace the color of all of the aforementioned pixels with color.
+        Return the modified image after performing the flood fill.
+"""
+
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         # if the cell is already visited and is colored then that's the end of possible coloring hence return the image
