62265f766baa2a738a56c96edcae6628210404c5
Sneha Patil <sneha.patil13sp@gmail.com>
2023-08-05 15:28:51 +0530

    Update 285. Inorder Successor in BST.py

--- Binary Tree/285. Inorder Successor in BST.py
+++ Binary Tree/285. Inorder Successor in BST.py
@@ -1,3 +1,10 @@
+"""
+    Question:
+        Given the root of a binary search tree and a node p in it, return the in-order successor of that node in the BST. 
+        If the given node has no in-order successor in the tree, return null.
+        The successor of a node p is the node with the smallest key greater than p.val.
+"""
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
