26053b8e28204c708dfdf6fd94c406bd4f3d03ee
Sneha Patil <sneha.patil13sp@gmail.com>
2023-08-05 15:54:23 +0530

    Update 77. Combinations.py

--- Backtracking/77. Combinations.py
+++ Backtracking/77. Combinations.py
@@ -1,3 +1,9 @@
+"""
+    Question:
+        Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+        You may return the answer in any order.
+"""
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         output = []
