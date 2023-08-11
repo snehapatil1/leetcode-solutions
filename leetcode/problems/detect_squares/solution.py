15030eb403e4a24a508505c8301910cde3dc8ace
Sneha Patil <sneha.patil13sp@gmail.com>
2023-08-11 13:41:29 +0000

    Sync LeetCode submission - Detect Squares (python3)

---
+++ leetcode/problems/detect_squares/solution.py
@@ -0,0 +1,26 @@
+class DetectSquares:
+
+    def __init__(self):
+        self.points = collections.defaultdict(int)
+
+    def add(self, point: List[int]) -> None:
+        self.points[tuple(point)] += 1
+
+    def count(self, point: List[int]) -> int:
+        count = 0
+        x1, y1 = point[0], point[1]
+
+        for (x2, y2), n in self.points.items():
+            x_dist, y_dist = abs(x1 - x2), abs(y1 - y2)
+            if x_dist == y_dist and x_dist > 0:
+                corner1 = (x1, y2)
+                corner2 = (x2, y1)
+                if corner1 in self.points and corner2 in self.points:
+                    count += n * self.points[corner1] * self.points[corner2]
+        return count
+
+
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
