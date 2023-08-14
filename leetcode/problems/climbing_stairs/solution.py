90d7a0117214ad8675ee13f751352111a4234e73
Sneha Patil <sneha.patil13sp@gmail.com>
2023-08-14 14:34:47 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- leetcode/problems/climbing_stairs/solution.py
+++ leetcode/problems/climbing_stairs/solution.py
@@ -1,10 +1,13 @@
 class Solution:
-    @cache
     def climbStairs(self, n: int) -> int:
         if n == 1:
-            return 1
+            return n
         
-        if n == 2:
-            return 2
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
         
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return dp[-1]
