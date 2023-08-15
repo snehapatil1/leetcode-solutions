29dedbcc0e70615205513d37fb1091d914ffdf86
Sneha Patil <sneha.patil13sp@gmail.com>
2023-08-15 12:31:51 +0000

    Sync LeetCode submission - Partition List (python3)

---
+++ leetcode/problems/partition_list/solution.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        before = before_head = ListNode(0) # before linked list to maintain smaller < x nodes
+        after = after_head = ListNode(0) # after linked list to maintain greater & equal >= x nodes
+
+        while head:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+            head = head.next
+        
+        after.next = None
+        before.next = after_head.next
+
+        return before_head.next
