6d110861307c95bf274b16abc9123d4133c5d786
Sneha Patil <sneha.patil13sp@gmail.com>
2023-08-18 16:33:15 +0000

    Sync LeetCode submission - The Number of Weak Characters in the Game (python3)

--- leetcode/problems/the_number_of_weak_characters_in_the_game/solution.py
+++ leetcode/problems/the_number_of_weak_characters_in_the_game/solution.py
@@ -1,17 +1,13 @@
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
-        weak_characters = 0
+        properties.sort(key=lambda x: (x[0], -x[1]))
+        weak = 0
+        queue = []
+
+        for character in range(len(properties)):
+            while queue and queue[0][0] < properties[character][1] and queue[0][1] < properties[character][0]:
+                weak += 1
+                heapq.heappop(queue)
+            heapq.heappush(queue, (properties[character][1], properties[character][0]))
         
-        properties.sort(key=lambda x:(-x[0],x[1]))
-            
-        max_attack = properties[0][0]
-        max_defense = properties[0][1]
-        
-        for character in properties:
-            if character[0]<max_attack and character[1]<max_defense:
-                weak_characters += 1
-            else:
-                max_attack = character[0]
-                max_defense = character[1]
-        
-        return weak_characters
+        return weak
