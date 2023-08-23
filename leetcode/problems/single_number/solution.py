3c6ad293566d3a4bebca87437dc74eaaa6ae51b3
Sneha Patil <sneha.patil13sp@gmail.com>
2023-08-23 17:22:51 +0000

    Sync LeetCode submission - Single Number (python3)

--- leetcode/problems/single_number/solution.py
+++ leetcode/problems/single_number/solution.py
@@ -1,12 +1,21 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hash_map = {}
-        for i in nums:
-            if i not in hash_map.keys():
-                hash_map.update({i: 1})
-            else:
-                hash_map[i] += 1
+        # hash_map = {}
+        # for i in nums:
+        #     if i not in hash_map.keys():
+        #         hash_map.update({i: 1})
+        #     else:
+        #         hash_map[i] += 1
         
-        for i in hash_map:
-            if hash_map[i] == 1:
-                return i
+        # for i in hash_map:
+        #     if hash_map[i] == 1:
+        #         return i
+
+        # ========== Bit Manipulation
+
+        xor = 0
+
+        for num in nums:
+            xor ^= num
+        
+        return xor
