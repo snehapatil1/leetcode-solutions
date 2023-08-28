d2cc63a45aff119f50299969b0d8284569c6551c
Sneha Patil <sneha.patil13sp@gmail.com>
2023-08-28 17:27:10 +0000

    Sync LeetCode submission - Reverse String (python3)

--- leetcode/problems/reverse_string/solution.py
+++ leetcode/problems/reverse_string/solution.py
@@ -3,4 +3,12 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        return s.reverse()
+        # return s.reverse()
+
+        left, right = 0, len(s) - 1
+        while left < right:
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left += 1
+            right -= 1
