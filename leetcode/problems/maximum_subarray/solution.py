fa573d90e8481b2f013919eb65e8d6efbb554062
Sneha Patil <sneha.patil13sp@gmail.com>
2023-09-05 14:57:11 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- leetcode/problems/maximum_subarray/solution.py
+++ leetcode/problems/maximum_subarray/solution.py
@@ -1,19 +1,30 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        length = len(nums)
-        if not length:
-            return 0
-        elif length == 1:
-            return nums[0]
+        maxSum = nums[0]
+
+        total = 0
+        for num in nums:
+            total += num
+            maxSum = max(maxSum, total)
+            if total < 0:
+                total = 0
+        return maxSum
+
+
+        # length = len(nums)
+        # if not length:
+        #     return 0
+        # elif length == 1:
+        #     return nums[0]
         
-        current_sum = 0
-        index_sum = []
-        index_sum.append(nums[0])
-        for ele in range(1, length):
-            current_sum = index_sum[ele-1] + nums[ele]
-            if current_sum > nums[ele]:
-                index_sum.append(current_sum)
-            else:
-                index_sum.append(nums[ele])
+        # current_sum = 0
+        # index_sum = []
+        # index_sum.append(nums[0])
+        # for ele in range(1, length):
+        #     current_sum = index_sum[ele-1] + nums[ele]
+        #     if current_sum > nums[ele]:
+        #         index_sum.append(current_sum)
+        #     else:
+        #         index_sum.append(nums[ele])
             
-        return max(index_sum)
+        # return max(index_sum)
