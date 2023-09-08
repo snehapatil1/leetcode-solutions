2dbd4272cc97756b47f4fc12dc15d83e516e77a6
Sneha Patil <sneha.patil13sp@gmail.com>
2023-09-08 11:47:59 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- leetcode/problems/valid_anagram/solution.py
+++ leetcode/problems/valid_anagram/solution.py
@@ -1,8 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_arr = list(s)
-        t_arr = list(t)
-        s_arr.sort()
-        t_arr.sort()
+        # s_arr = list(s)
+        # t_arr = list(t)
+        # s_arr.sort()
+        # t_arr.sort()
 
-        return s_arr == t_arr
+        # return s_arr == t_arr
+
+        # return sorted(s) == sorted(t)
+
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = defaultdict(int), defaultdict(int)
+
+        for idx in range(len(s)):
+            countS[s[idx]] = countS.get(s[idx], 0) + 1
+            countT[t[idx]] = countT.get(t[idx], 0) + 1
+        
+        return countS == countT
