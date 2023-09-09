ac7cc5f505be2dda65f02e7b615d53e6da903409
Sneha Patil <sneha.patil13sp@gmail.com>
2023-09-09 15:45:41 +0000

    Sync LeetCode submission - Top K Frequent Words (python3)

--- leetcode/problems/top_k_frequent_words/solution.py
+++ leetcode/problems/top_k_frequent_words/solution.py
@@ -1,25 +1,30 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        word_count_map = {}
-        for word in words:
-            if word not in word_count_map:
-                word_count_map[word] = 0
-            word_count_map[word] += 1
+        counts = Counter(words)
+        heap = [(-count, word) for word, count in counts.items()]
+        heapq.heapify(heap)
+        return [heapq.heappop(heap)[1] for _ in range(k)]
         
-        count_word_map = {}
-        for word in word_count_map:
-            if word_count_map[word] not in count_word_map:
-                count_word_map[word_count_map[word]] = []
-            count_word_map[word_count_map[word]].append(word)
-            count_word_map[word_count_map[word]].sort()
-
-        count_word_map = dict(sorted(count_word_map.items(), reverse=True))
-
-        op = []
-        for key in count_word_map:
-            for val in count_word_map[key]:
-                if k > 0:
-                    op.append(val)
-                    k -= 1
+        # word_count_map = {}
+        # for word in words:
+        #     if word not in word_count_map:
+        #         word_count_map[word] = 0
+        #     word_count_map[word] += 1
         
-        return op
+        # count_word_map = {}
+        # for word in word_count_map:
+        #     if word_count_map[word] not in count_word_map:
+        #         count_word_map[word_count_map[word]] = []
+        #     count_word_map[word_count_map[word]].append(word)
+        #     count_word_map[word_count_map[word]].sort()
+
+        # count_word_map = dict(sorted(count_word_map.items(), reverse=True))
+
+        # op = []
+        # for key in count_word_map:
+        #     for val in count_word_map[key]:
+        #         if k > 0:
+        #             op.append(val)
+        #             k -= 1
+        
+        # return op
