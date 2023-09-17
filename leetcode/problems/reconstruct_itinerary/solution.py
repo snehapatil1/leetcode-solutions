1f2feff2e0e24f808b8dd7ed23dbac437c156bf7
Sneha Patil <sneha.patil13sp@gmail.com>
2023-09-17 06:46:08 +0000

    Sync LeetCode submission - Reconstruct Itinerary (python3)

--- leetcode/problems/reconstruct_itinerary/solution.py
+++ leetcode/problems/reconstruct_itinerary/solution.py
@@ -1,22 +1,27 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        graph = {}
-        for src, dst in tickets:
-            if src in graph:
-                graph[src].append(dst)
-            else:
-                graph[src] = [dst]
-
-        for src in graph.keys():
-            graph[src].sort(reverse=True)
-        stack = []
-        res = []
-        stack.append("JFK")
+        graph = defaultdict(list)
+        tickets.sort()
+        for src, dest in tickets:
+            graph[src].append(dest)
         
-        while len(stack) > 0:
-            elem = stack[-1]
-            if elem in graph and len(graph[elem]) > 0: 
-                stack.append(graph[elem].pop())
-            else:
-                res.append(stack.pop())
-        return res[::-1]
+        path = ["JFK"]
+
+        def dfs(src):
+            if len(path) == len(tickets) + 1:
+                return path
+            if src not in graph:
+                return False
+            
+            temp = list(graph[src])
+            for idx, dest in enumerate(temp):
+                graph[src].pop(idx)
+                path.append(dest)
+                if dfs(dest):
+                    return True
+                graph[src].insert(idx, dest)
+                path.pop()
+            return False
+        
+        dfs("JFK")
+        return path
