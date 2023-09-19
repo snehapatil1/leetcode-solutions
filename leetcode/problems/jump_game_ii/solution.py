c3df64d5b272b05acdb3ab099f3d6324b60cd2c2
Sneha Patil <sneha.patil13sp@gmail.com>
2023-09-19 13:40:01 +0000

    Sync LeetCode submission - Jump Game II (python3)

--- leetcode/problems/jump_game_ii/solution.py
+++ leetcode/problems/jump_game_ii/solution.py
@@ -1,15 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
-            return 0
-        
-        dp = [0] * n
-        j = 0
-
-        for i in range(1, n):
-            while j + nums[j] < i:
-                j += 1
-            dp[i] = dp[j] + 1
-        
-        return dp[-1]
+        start, nextJump = 0, 0
+        maxJump, minJumps = 0, 0
+        while nextJump < (len(nums) - 1):
+            for i in range(start, nextJump + 1):
+                maxJump = max(maxJump, nums[i] + i)
+            start = nextJump + 1
+            nextJump = maxJump
+            minJumps += 1
+        return minJumps
