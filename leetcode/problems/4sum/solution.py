6619b1a76506af67c94048b457e95a51e3b94996
Sneha Patil <sneha.patil13sp@gmail.com>
2023-10-08 16:29:18 +0000

    Sync LeetCode submission - 4Sum (python3)

--- leetcode/problems/4sum/solution.py
+++ leetcode/problems/4sum/solution.py
@@ -1,52 +1,32 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        is_satisfied = self.areConstraintsSatisfied(nums, target)
-        if (not is_satisfied):
-            return False
-        
         nums.sort()
-        n = len(nums)
-        ans = []
-        
-        i = 0
-        while i<n:
-            j=i+1
-            while j<n:
-                end=n-1
-                strt=j+1
-                nTarget=target-nums[i]-nums[j]
-                
-                while strt<end:                    
-                    if nums[strt]+nums[end]==nTarget:
-                        ans.append([nums[i],nums[j],nums[strt],nums[end]])
 
-                        third=nums[strt]
-                        fourth=nums[end]
-                        while strt<end and nums[strt]==third:
-                            strt+=1
-                        while strt<end and nums[end]==fourth:
-                            end-=1
-                    elif nums[strt]+nums[end]>nTarget:
-                        end-=1
-                    else:
-                        strt+=1
-                t=nums[j]
-                while j<n and nums[j]==t:
-                    j+=1
-            t2=nums[i]
-            while i<n and nums[i]==t2:
-                i+=1
-        return ans
-    
-    def areConstraintsSatisfied(self, nums, target):
-        if (len(nums) < 1) or (len(nums) > 200):
-            return False
+        def twoSum(nums, target):
+            output = []
+            visited = set()
+
+            for i in range(len(nums)):
+                if len(output) == 0 or output[-1][1] != nums[i]:
+                    if target - nums[i] in visited:
+                        output.append([target - nums[i], nums[i]])
+                visited.add(nums[i])
+            
+            return output
         
-        for ele in nums:
-            if (ele < -10**9) or (ele > 10**9):
-                return False
+        def nSum(nums, target, n):
+            output = []
+            if not nums:
+                return output
+            
+            if n == 2:
+                return twoSum(nums, target)
+            
+            for i in range(len(nums)):
+                if i == 0 or nums[i - 1] != nums[i]:
+                    for subset in nSum(nums[i + 1:], target - nums[i], n - 1):
+                        output.append([nums[i]] + subset)
+            
+            return output
         
-        if (target < -10**9) or (target > 10**9):
-            return False
-        
-        return True
+        return nSum(nums, target, 4)
