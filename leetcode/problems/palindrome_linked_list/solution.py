8afa29fbbb96d7b4f991d4eb36c07acbbedad93a
Sneha Patil <sneha.patil13sp@gmail.com>
2023-10-09 15:02:32 +0000

    Sync LeetCode submission - Palindrome Linked List (python3)

--- leetcode/problems/palindrome_linked_list/solution.py
+++ leetcode/problems/palindrome_linked_list/solution.py
@@ -5,11 +5,13 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        reversed_list = []
-        current = head
-        while current:
-            reversed_list.append(current.val)
-            current = current.next
-        return reversed_list == reversed_list[::-1]
-                
-                
+        if not head:
+            return False
+        
+        lList = []
+
+        while head:
+            lList.append(head.val)
+            head = head.next
+        
+        return lList == lList[::-1]
