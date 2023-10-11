71e6e279a87a4c063cbd2e046653bb0f71c32677
Sneha Patil <sneha.patil13sp@gmail.com>
2023-10-11 16:14:31 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

--- leetcode/problems/valid_palindrome/solution.py
+++ leetcode/problems/valid_palindrome/solution.py
@@ -1,6 +1,17 @@
-import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new_s = re.sub(r"[^a-zA-Z0-9\\s+]", "", s).lower()
-        return new_s == new_s[::-1]
+        p1, p2 = 0, len(s) - 1
+
+        while p1 <= p2:
+            if (s[p1].isalpha() or s[p1].isdigit()) and (s[p2].isalpha() or s[p2].isdigit()):
+                if s[p1].lower() != s[p2].lower():
+                    return False
+                p1 += 1
+                p2 -= 1
+            elif not s[p1].isalpha() and not s[p1].isdigit():
+                p1 += 1
+            elif not s[p2].isalpha() and not  s[p2].isdigit():
+                p2 -= 1
+        
+        return True
