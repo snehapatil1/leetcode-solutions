6f20212938e7dbb4c7a07095d2c12d53769cdaea
Sneha Patil <sneha.patil13sp@gmail.com>
2023-10-15 13:22:58 +0000

    Sync LeetCode submission - Time Needed to Inform All Employees (python3)

--- leetcode/problems/time_needed_to_inform_all_employees/solution.py
+++ leetcode/problems/time_needed_to_inform_all_employees/solution.py
@@ -1,12 +1,26 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        subordinates = defaultdict(set)
+        subordinates = defaultdict(list)
         
-        def dfs(boss):
-            return informTime[boss] + max((dfs(emp) for emp in subordinates[boss]), default=0)
+        # def dfs(boss):
+        #     return informTime[boss] + max((dfs(emp) for emp in subordinates[boss]), default=0)
         
         
+        # for emp, boss in enumerate(manager):
+        #     subordinates[boss].add(emp)
+        
+        # return dfs(headID)
+
         for emp, boss in enumerate(manager):
-            subordinates[boss].add(emp)
+            subordinates[boss].append(emp)
         
-        return dfs(headID)
+        queue = deque([(headID, 0)])
+        totalTime = 0
+
+        while queue:
+            emp, time = queue.popleft()
+            totalTime = max(totalTime, time)
+            for subordinate in subordinates[emp]:
+                queue.append((subordinate, time + informTime[emp]))
+        
+        return totalTime
