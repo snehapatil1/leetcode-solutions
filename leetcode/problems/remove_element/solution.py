6dcb438c0cc045057c824d02617c588780e10537
Sneha Patil <sneha.patil13sp@gmail.com>
2023-10-22 16:14:45 +0000

    Sync LeetCode submission - Remove Element (python3)

--- leetcode/problems/remove_element/solution.py
+++ leetcode/problems/remove_element/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         count = nums.count(val)
-
+        
         while val in nums:
             nums.remove(val)
         
         for i in range(count):
             nums.append('_')
         
-        return (len(nums) - count)
+        return len(nums) - count
