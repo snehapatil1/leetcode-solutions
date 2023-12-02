45bc71957b508119df52b69893abd39f0277579d
Sneha Patil <sneha.patil13sp@gmail.com>
2023-12-02 16:04:37 +0000

    Sync LeetCode submission - Kth Smallest Element in a BST (python3)

--- leetcode/problems/kth_smallest_element_in_a_bst/solution.py
+++ leetcode/problems/kth_smallest_element_in_a_bst/solution.py
@@ -6,20 +6,7 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        smallest_nums = []
-
-        smallest_nums = self.dfs(root, smallest_nums)
-        smallest_nums.sort()
-
-        return smallest_nums[k-1]
+        def traverse(node):
+            return traverse(node.left) + [node.val] + traverse(node.right) if node else []
         
-            
-    def dfs(self, root, smallest_nums):
-        if not root:
-            return []
-        
-        smallest_nums = self.dfs(root.left, smallest_nums)
-        smallest_nums.append(root.val)
-        smallest_nums += self.dfs(root.right, smallest_nums)
-
-        return smallest_nums
+        return traverse(root)[k - 1]
