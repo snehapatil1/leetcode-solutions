06f6ac1897eb9252cea0c8a0585eb569b47b09ec
Sneha Patil <sneha.patil13sp@gmail.com>
2023-12-30 14:57:08 +0000

    Sync LeetCode submission - Redistribute Characters to Make All Strings Equal (python3)

---
+++ leetcode/problems/redistribute_characters_to_make_all_strings_equal/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        counts = [0] * 26
+        for word in words:
+            for c in word:
+                counts[ord(c) - ord('a')] += 1
+        
+        n = len(words)
+        for val in counts:
+            if val % n != 0:
+                return False
+
+        return True
