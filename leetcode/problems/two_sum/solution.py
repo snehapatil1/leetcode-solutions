f99a2aa1eb01b3bff5e6e5ebbebbc57fbd82c6c2
Sneha Patil <sneha.patil13sp@gmail.com>
2025-01-22 04:01:04 +0000

    Sync LeetCode submission - Two Sum (python3)

--- leetcode/problems/two_sum/solution.py
+++ leetcode/problems/two_sum/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_map = collections.defaultdict(int)
-        for idx, num in enumerate(nums):
-            complement = target - num
-            if complement in hash_map:
-                return [idx, hash_map[complement]]
-            hash_map[num] = idx
+        hmap = {}
+        for i in range(len(nums)):
+            num2 = target - nums[i]
+            if num2 in hmap:
+                return [hmap[num2], i]
+            hmap[nums[i]] = i
