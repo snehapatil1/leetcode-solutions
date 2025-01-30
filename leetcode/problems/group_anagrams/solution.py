6c59f62c301d689a3ed70a6bec1f01e4f1eadddb
Sneha Patil <sneha.patil13sp@gmail.com>
2025-01-30 23:07:23 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- leetcode/problems/group_anagrams/solution.py
+++ leetcode/problems/group_anagrams/solution.py
@@ -1,16 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams_map = {}
-
-        for word in strs:
-            sorted_string = ''.join(sorted(word))
-            if sorted_string not in anagrams_map.keys():
-                anagrams_map[sorted_string] = []
-            anagrams_map[sorted_string].append(word)
-        
-        output = []
-        for key in anagrams_map:
-            output.append(anagrams_map[key])
-        
-        return output
-        
+        anagrams = defaultdict(list)
+        for string in strs:
+            sorted_str = "".join(sorted(string))
+            anagrams[sorted_str].append(string)
+        return list(anagrams.values())
