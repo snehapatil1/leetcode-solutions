b7d8d82442d81d8f583b6edadde3c50b482d9f58
Sneha Patil <sneha.patil13sp@gmail.com>
2025-02-04 21:48:09 +0000

    Sync LeetCode submission - Reorder List (python3)

--- leetcode/problems/reorder_list/solution.py
+++ leetcode/problems/reorder_list/solution.py
@@ -3,30 +3,36 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
+        # slow will reach mid when fast will reach end of list
         slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
-        second = slow.next
-        prev = None
+        # slow's next node will be list2 start
+        list2 = slow.next
         slow.next = None
-
-        while second:
-            temp = second.next
-            second.next = prev
-            prev = second
-            second = temp
+        prev = None
         
-        first, second = head, prev
-        while second:
-            temp1, temp2 = first.next, second.next
-            first.next = second
-            second.next = temp1
-            first = temp1
-            second = temp2
+        # reverse list2
+        while list2:
+            nxt = list2.next
+            list2.next = prev
+            prev = list2
+            list2 = nxt
+        
+        # list1 will be original head and list2 will be reversed list
+        list1, list2 = head, prev
+
+        # merge list1 and list2
+        while list2:
+            l1nxt, l2nxt = list1.next, list2.next
+            list1.next = list2
+            list2.next = l1nxt
+            list1 = l1nxt
+            list2 = l2nxt
+        
+
