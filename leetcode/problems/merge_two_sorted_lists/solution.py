7467cabd078d3b70cf1a78e50944ed3e43c66724
Sneha Patil <sneha.patil13sp@gmail.com>
2025-02-04 20:58:28 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

--- leetcode/problems/merge_two_sorted_lists/solution.py
+++ leetcode/problems/merge_two_sorted_lists/solution.py
@@ -3,35 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # create linked list to do merging operation
-        mergedList = ListNode()
-        # create another linked list to maintain copy of merged linked list
-        newList = mergedList
+        merged = ListNode()
+        dummy = merged
 
         while list1 and list2:
-            if list1.val < list2.val:
-                # if list1 val is smaller than list2 val then set that in merged list and iterate to next element of list1
-                mergedList.next = list1
+            if list1.val <= list2.val:
+                merged.next = ListNode(list1.val)
                 list1 = list1.next
             else:
-                # if list2 val is smaller than list1 val then set that in merged list and iterate to next element of list2
-                mergedList.next = list2
+                merged.next = ListNode(list2.val)
                 list2 = list2.next
-            
-            # move pointer to next node of merged list
-            mergedList = mergedList.next
-
-        # if there are nodes left in list1 then add all as it is to the merged list
+            merged = merged.next
+        
         if list1:
-            mergedList.next = list1
+            merged.next = list1
         
-        # if there are nodes left in list2 then add all as it is to the merged list
         if list2:
-            mergedList.next = list2
+            merged.next = list2
         
-        # return another copy of list which is pointing to merged list head
-        return newList.next
-
-
+        return dummy.next
