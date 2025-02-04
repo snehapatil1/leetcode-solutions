38c1421956aca6078964a41061ef933a5f3e0c3e
Sneha Patil <sneha.patil13sp@gmail.com>
2025-02-04 19:13:35 +0000

    Sync LeetCode submission - Find Minimum in Rotated Sorted Array (python3)

--- leetcode/problems/find_minimum_in_rotated_sorted_array/solution.py
+++ leetcode/problems/find_minimum_in_rotated_sorted_array/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        left, right = 0, len(nums) - 1
+        n = len(nums)
+        left, right = 0, n - 1
 
         while left < right:
-            mid = left + (right - left) // 2
-            
-            if nums[mid] > nums[right]:
-                left = mid + 1
-            else:
+            mid = (left + right) // 2
+            if nums[mid] < nums[right]:
                 right = mid
+            else:
+                left = mid + 1
         return nums[left]
