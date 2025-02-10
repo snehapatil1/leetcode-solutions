8765967c0881ec70e34025e5bfebbacacf90d37e
Sneha Patil <sneha.patil13sp@gmail.com>
2025-02-10 23:55:26 +0000

    Sync LeetCode submission - Same Tree (python3)

--- leetcode/problems/same_tree/solution.py
+++ leetcode/problems/same_tree/solution.py
@@ -6,9 +6,21 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        elif p and q and p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        else:
-            return False
+        q1, q2 = deque([p]), deque([q])
+
+        while q1 and q2:
+            for _ in range(len(q1)):
+                nodeP = q1.popleft()
+                nodeQ = q2.popleft()
+
+                if not nodeP and not nodeQ:
+                    continue
+                if not nodeP or not nodeQ or nodeP.val != nodeQ.val:
+                    return False
+                
+                q1.append(nodeP.left)
+                q1.append(nodeP.right)
+                q2.append(nodeQ.left)
+                q2.append(nodeQ.right)
+        
+        return True
