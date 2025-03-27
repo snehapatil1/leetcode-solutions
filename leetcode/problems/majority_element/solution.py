64db9428ca6cc4fb6d25bc7b1ab38b12db34fd4c
Sneha Patil <sneha.patil13sp@gmail.com>
2025-03-27 17:12:10 +0000

    Sync LeetCode submission - Majority Element (python3)

--- leetcode/problems/majority_element/solution.py
+++ leetcode/problems/majority_element/solution.py
@@ -1,8 +1,11 @@
-import math
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        majority_times = math.ceil(len(nums) / 2)
-        nums_set = set(nums)
-        for num in nums_set:
-            if nums.count(num) >= majority_times:
-                return num
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+        
+        return candidate
