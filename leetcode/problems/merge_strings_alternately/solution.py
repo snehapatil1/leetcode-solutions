adbd3772b0c0c551a7c3d9dce8481bab7ce06ba1
Sneha Patil <sneha.patil13sp@gmail.com>
2025-03-27 21:49:40 +0000

    Sync LeetCode submission - Merge Strings Alternately (python3)

--- leetcode/problems/merge_strings_alternately/solution.py
+++ leetcode/problems/merge_strings_alternately/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         merged = ""
+        i, j = len(word1), len(word2)
         n = max(len(word1), len(word2))
-        for i in range(n):
-            if i < len(word1):
-                merged += word1[i]
-            if i < len(word2):
-                merged += word2[i]
+        for x in range(n):
+            if x < i:
+                merged += word1[x]
+            if x < j:
+                merged += word2[x]
         return merged
