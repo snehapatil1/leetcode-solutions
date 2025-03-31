f2517b974cff161d73c3753df83bd1f467699a05
Sneha Patil <sneha.patil13sp@gmail.com>
2025-03-31 16:52:36 +0000

    Sync LeetCode submission - H-Index (python3)

--- leetcode/problems/h-index/solution.py
+++ leetcode/problems/h-index/solution.py
@@ -1,25 +1,9 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort()
+        h = 0
         n = len(citations)
-        if n == 1 and citations[0] == 0:
-            return 0
-        if n == 1 and citations[0] != 0:
-            return 1
-        
-        h = 1 if citations[0] != 0 else 0
-        p1, p2 = 0, 1
-        minSoFar = citations[0]
-
-        while p1 <= p2 < n:
-            required = p2 - p1 + 1
-            if minSoFar >= required:
-                p2 += 1
-                h = max(h, required)
-            else:
-                if p2 == p1: p2 += 1
-                p1 += 1
-            if p1 < n:
-                minSoFar = citations[p1]
-        
-        return h
+        citations.sort()
+        for i, citation in enumerate(citations):
+            if citation >= n - i:
+                return n - i
+        return 0
