8a21d809f6e2e9a66b11d2243a49b5f0951dffcb
Sneha Patil <sneha.patil13sp@gmail.com>
2025-04-01 04:15:45 +0000

    Sync LeetCode submission - Roman to Integer (python3)

--- leetcode/problems/roman_to_integer/solution.py
+++ leetcode/problems/roman_to_integer/solution.py
@@ -1,26 +1,17 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        self.hash_table = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        
-        roman = list(s)
-        number = 0
-        
-        for idx, letter in enumerate(roman):
-            num = self.hash_table[letter]
-            
-            if idx+1 <= len(roman)-1:
-                if (letter == 'I' and roman[idx+1] in ['V', 'X'] \
-                   or letter == 'X' and roman[idx+1] in ['L', 'C'] \
-                   or letter == 'C' and roman[idx+1] in ['D', 'M']):
-                    num = -num
-            number += num
-        
-        return number
+        hmap = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        pairs = {'I': ['V', 'X'], 'X': ['L', 'C'], 'C': ['D', 'M']}
+        num = 0
+        n = len(s)
+
+        for idx in range(n):
+            if s[idx] in pairs.keys():
+                if idx < n - 1 and s[idx + 1] in pairs[s[idx]]:
+                    num -= hmap[s[idx]]
+                else:
+                    num += hmap[s[idx]]
+            else:
+                num += hmap[s[idx]]
+
+        return num
