bf8d992291e2cf98f3ecafbcb1e6bdf76f7eaa1a
Sneha Patil <sneha.patil13sp@gmail.com>
2025-04-09 21:13:16 +0000

    Sync LeetCode submission - Flood Fill (python3)

--- leetcode/problems/flood_fill/solution.py
+++ leetcode/problems/flood_fill/solution.py
@@ -1,30 +1,20 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        # if the cell is already visited and is colored then that's the end of possible coloring hence return the image
-        if image[sr][sc] == color:
+        rows = len(image)
+        cols = len(image[0])
+        clr = image[sr][sc]
+        if clr == color:
             return image
-        
-        self.fill(image, sr, sc, color, image[sr][sc])
-        
+        def dfs(r, c):
+            if image[r][c] == clr:
+                image[r][c] = color
+                if r >= 1:
+                    dfs(r - 1, c)
+                if r + 1 < rows:
+                    dfs(r + 1, c)
+                if c >= 1:
+                    dfs(r, c - 1)
+                if c + 1 < cols:
+                    dfs(r, c + 1)
+        dfs(sr, sc)
         return image
-    
-    def fill(self, image, sr, sc, color, cur):
-        # if the row or column doesn't exist as per matrix size then return
-        if sr < 0 or sr >= len(image) or sc < 0 or sc >= len(image[0]):
-            return
-        
-        # if current cel value is not same as desired value then no need to cover borders of that cell hence return
-        if cur != image[sr][sc]:
-            return
-        
-        # set current cell to color value
-        image[sr][sc] = color
-        
-        # cover immediate top cell
-        self.fill(image, sr-1, sc, color, cur)
-        # cover immediate bottom cell
-        self.fill(image, sr+1, sc, color, cur)
-        # cover immediate left cell
-        self.fill(image, sr, sc-1, color, cur)
-        # cover immediate right cell
-        self.fill(image, sr, sc+1, color, cur)
