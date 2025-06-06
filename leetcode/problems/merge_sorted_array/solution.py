2f0f6fee978c63aad3a3d7d714621a74023266f3
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-06 17:14:23 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

--- leetcode/problems/merge_sorted_array/solution.py
+++ leetcode/problems/merge_sorted_array/solution.py
@@ -3,12 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        temp = nums1[:m]
-        nums1_index, nums2_index = 0, 0
-        for write_index in range(m + n):
-            if nums2_index >= n or (nums1_index < m and temp[nums1_index] < nums2[nums2_index]):
-                nums1[write_index] = temp[nums1_index]
-                nums1_index += 1
+        ptr1, ptr2 = m - 1, n - 1
+        for idx in range(n + m - 1, -1, -1):
+            if ptr2 < 0:
+                break
+            if ptr1 >= 0 and nums1[ptr1] > nums2[ptr2]:
+                nums1[idx] = nums1[ptr1]
+                ptr1 -= 1
             else:
-                nums1[write_index] = nums2[nums2_index]
-                nums2_index += 1
+                nums1[idx] = nums2[ptr2]
+                ptr2 -= 1
+
+
+        
