8fac1905483a701a1afeeca5d702b2504a1d774a
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-06 21:59:45 +0000

    Sync LeetCode submission - LRU Cache (python3)

--- leetcode/problems/lru_cache/solution.py
+++ leetcode/problems/lru_cache/solution.py
@@ -1,30 +1,54 @@
+class ListNode:
+    def __init__(self, key: int, val: int):
+        self.key = key
+        self.val = val
+        self.head = None
+        self.tail = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
-        self.queue = []
+        self.dic = {}
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-        if key in self.cache.keys():
-            self.queue.remove(key)
-            self.queue.append(key)
-            return self.cache[key]
-        return -1
+        if key not in self.dic:
+            return -1
+        
+        node = self.dic[key]
+        self.remove(node)
+        self.add(node)
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        keys = self.cache.keys()
-        if key in keys:
-            self.cache[key] = value
-            self.queue.remove(key)
-            self.queue.append(key)
-        else:
-            if len(keys) >= self.capacity:
-                del self.cache[self.queue[0]]
-                self.queue.pop(0)
-            self.cache.update({key : value})
-            self.queue.append(key)
+        if key in self.dic:
+            node = self.dic[key]
+            self.remove(node)
+        
+        node = ListNode(key, value)
+        self.add(node)
 
+        if len(self.dic) > self.capacity:
+            delete_node = self.head.next
+            self.remove(delete_node)
+    
+    def add(self, node: ListNode) -> None:
+        previousEnd = self.tail.prev
+        previousEnd.next = node
+        node.prev = previousEnd
+        node.next = self.tail
+        self.tail.prev = node
+
+        self.dic[node.key] = node
+
+    def remove(self, node: ListNode) -> None:
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        del self.dic[node.key]
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
