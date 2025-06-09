60d73c6b8ac4dc026408f0291aa48d7b63cff87c
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-09 23:12:22 +0000

    Sync LeetCode submission - Reorganize String (python3)

--- leetcode/problems/reorganize_string/solution.py
+++ leetcode/problems/reorganize_string/solution.py
@@ -1,36 +1,23 @@
-import heapq
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        counts = collections.Counter(s)
         output = ""
-        heap = []
-        
-        for ch in counts:
-            heapq.heappush(heap, (-counts[ch], ch))
+        counts = Counter(s)
+        heap = [(-val, ch) for ch, val in counts.items()]
         heapq.heapify(heap)
 
-        while len(heap) >= 2:
-            remaining1, ch1 = heapq.heappop(heap)
-            remaining1 = -remaining1
-            remaining2, ch2 = heapq.heappop(heap)
-            remaining2 = -remaining2
-            
-            if output and output[-1] == ch1: return ""
-            output += ch1
+        while heap:
+            first_cnt, first_ch = heapq.heappop(heap)
+            if not output or output[-1] != first_ch:
+                output += first_ch
+                if first_cnt + 1 != 0:
+                    heapq.heappush(heap, (first_cnt + 1, first_ch))
+            else:
+                if not heap:
+                    return ""
+                second_cnt, second_ch = heapq.heappop(heap)
+                output += second_ch
+                if second_cnt + 1 != 0:
+                    heapq.heappush(heap, (second_cnt + 1, second_ch))
+                heapq.heappush(heap, (first_cnt, first_ch))
 
-            if remaining1 > 1:
-                heapq.heappush(heap, (-(remaining1 - 1), ch1))
-            
-            if output and output[-1] == ch2: return ""
-            output += ch2
-            
-            if remaining2 > 1:
-                heapq.heappush(heap, (-(remaining2 - 1), ch2))
-        
-        if heap:
-            remaining, ch = heapq.heappop(heap)
-            if -remaining > 1:
-                return ""
-            output += ch
-        
         return output
