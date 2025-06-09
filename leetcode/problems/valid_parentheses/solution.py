e60aadba875149a17029a89c581b44dca5d92f45
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-09 18:40:09 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- leetcode/problems/valid_parentheses/solution.py
+++ leetcode/problems/valid_parentheses/solution.py
@@ -1,13 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        pairs = {'(': ')', '{': '}', '[': ']'}
-        reverse_pair = {')': '(', '}': '{', ']': '['}
-        for i in s:
-            if i in pairs.keys():
-                stack.append(i)
-            else:
-                if (not stack) or (stack[-1] != reverse_pair[i]):
-                    return False
-                stack.pop()
-        return not stack
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        
+        for ch in s:
+            if stack:
+                if ch in pairs:
+                    if pairs[ch] != stack[-1]:
+                        return False
+                    stack.pop()
+                    continue
+            stack.append(ch)
+        
+        return stack == []
