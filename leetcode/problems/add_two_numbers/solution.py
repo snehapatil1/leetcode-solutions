afdef9d772ec9d58270cf22d8a01f5ae32d57631
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-11 01:13:35 +0000

    Sync LeetCode submission - Add Two Numbers (python3)

--- leetcode/problems/add_two_numbers/solution.py
+++ leetcode/problems/add_two_numbers/solution.py
@@ -4,36 +4,37 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def create_node(self, new_l, val):
+        node = ListNode(val)
+        node.next = None
+        new_l.next = node
+        new_l = new_l.next
+        return new_l
+    
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        newList = ListNode(0)
-        dummy = newList
-        total, carry = 0, 0
+        dummy = ListNode(0)
+        new_list = dummy
+        carry = 0
 
         while l1 or l2:
-            num1 = int(l1.val) if l1 else 0
-            num2 = int(l2.val) if l2 else 0
-            
-            res = (num1 + num2)
-            if carry > 0:
-                res += carry
+            l1_num = l1.val if l1 else 0
+            l2_num = l2.val if l2 else 0
+
+            new_num = l1_num + l2_num + carry
+            if new_num > 9:
+                carry = new_num // 10
+                new_num = new_num % 10
+            else:
                 carry = 0
             
-            total = res % 10
-            carry = res // 10
-
-            node = ListNode(total)
-            dummy.next = node
-            dummy = node
+            dummy = self.create_node(dummy, new_num)
 
             if l1:
                 l1 = l1.next
-            
             if l2:
                 l2 = l2.next
         
-        if carry > 0:
-            node = ListNode(carry)
-            dummy.next = node
-            dummy = node
-        
-        return newList.next
+        if carry:
+            dummy = self.create_node(dummy, carry)
+
+        return new_list.next
