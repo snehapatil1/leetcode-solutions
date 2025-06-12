c5cfa4deeca063b56f061a677944eddb5a24ad7a
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-12 18:14:19 +0000

    Sync LeetCode submission - Palindrome Number (python3)

--- leetcode/problems/palindrome_number/solution.py
+++ leetcode/problems/palindrome_number/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
-        x_str = str(x)
-        x_arr = list(x_str)
-        x_arr.reverse()
-        x_arr_str = ''.join(x_arr)
-        if str(x) == x_arr_str:
-            return True
-        else:
-            return False
+        
+        reverted = 0
+        while x > reverted:
+            reverted = reverted * 10 + x % 10
+            x //= 10
+        
+        return x == reverted or x == reverted//10
