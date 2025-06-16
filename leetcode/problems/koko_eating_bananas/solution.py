6ab1b4059de236d49942272339e59f07b6876ba8
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-16 22:58:08 +0000

    Sync LeetCode submission - Koko Eating Bananas (python3)

--- leetcode/problems/koko_eating_bananas/solution.py
+++ leetcode/problems/koko_eating_bananas/solution.py
@@ -1,17 +1,21 @@
+from math import ceil
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        piles.sort()
-        start, end = 1, max(piles)
+        n = len(piles)
+
+        if n == h:
+            return max(piles)
         
-        while start < end:
-            mid = (start + end) // 2
-            time = 0
-            for pile in piles:
-                time += math.ceil(pile / mid)
-            
-            if time <= h:
-                end = mid
+        l, r = 1, max(piles)
+
+        while l < r:
+            mid = (l + r) // 2
+            count = 0
+            for p in piles:
+                count += ceil(p / mid)
+            if count <= h:
+                r = mid
             else:
-                start = mid + 1
+                l = mid + 1
         
-        return end
+        return r
