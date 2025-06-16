f894d905ab32b29e80efbb293a4ee2f0f645112c
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-16 17:33:34 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- leetcode/problems/group_anagrams/solution.py
+++ leetcode/problems/group_anagrams/solution.py
@@ -1,7 +1,25 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)
-        for string in strs:
-            sorted_str = "".join(sorted(string))
-            anagrams[sorted_str].append(string)
-        return list(anagrams.values())
+        hmap = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            hmap[tuple(count)].append(word)
+
+        return list(hmap.values())
+
+        # hmap = {}
+        # output = []
+
+        # for word in strs:
+        #     sorted_word = "".join(sorted(word))
+        #     if sorted_word not in hmap:
+        #         hmap[sorted_word] = []
+        #     hmap[sorted_word].append(word)
+        
+        # for key in hmap:
+        #     output.append(hmap[key])
+
+        # return output
