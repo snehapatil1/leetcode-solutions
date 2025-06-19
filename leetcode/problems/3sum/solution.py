96e80ba44e0326e8f6ffe8ac8632c60d5d3ec5f3
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-19 19:28:20 +0000

    Sync LeetCode submission - 3Sum (python3)

--- leetcode/problems/3sum/solution.py
+++ leetcode/problems/3sum/solution.py
@@ -1,15 +1,16 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         output = set()
-        visited1, visited2 = set(), {}
-        n = len(nums)
+        visited1 = set()
+        visited2 = defaultdict(int)
 
         for idx1, num1 in enumerate(nums):
             if num1 not in visited1:
                 visited1.add(num1)
                 for idx2, num2 in enumerate(nums[idx1 + 1:]):
-                    num3 = -num1 - num2
+                    num3 = -num1 -num2
                     if num3 in visited2 and visited2[num3] == idx1:
                         output.add(tuple(sorted([num1, num2, num3])))
                     visited2[num2] = idx1
+
         return [list(op) for op in output]
