2b4e51c75feacf9d082da6b5f2dee3fda3600cdd
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-19 22:19:50 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- leetcode/problems/group_anagrams/solution.py
+++ leetcode/problems/group_anagrams/solution.py
@@ -4,22 +4,8 @@ class Solution:
 
         for word in strs:
             count = [0] * 26
-            for c in word:
-                count[ord(c) - ord('a')] += 1
+            for ch in word:
+                count[ord(ch) - ord('a')] += 1
             hmap[tuple(count)].append(word)
 
         return list(hmap.values())
-
-        # hmap = {}
-        # output = []
-
-        # for word in strs:
-        #     sorted_word = "".join(sorted(word))
-        #     if sorted_word not in hmap:
-        #         hmap[sorted_word] = []
-        #     hmap[sorted_word].append(word)
-        
-        # for key in hmap:
-        #     output.append(hmap[key])
-
-        # return output
