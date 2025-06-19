bdb39de366925631d6a66ac80d1239f1089ab26f
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-19 22:39:54 +0000

    Sync LeetCode submission - Jump Game (python3)

--- leetcode/problems/jump_game/solution.py
+++ leetcode/problems/jump_game/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n = len(nums)
-        last_position = n - 1
-        for i in range(n - 1, -1, -1):
-            if i + nums[i] >= last_position:
-                last_position = i
+        last_position = len(nums) - 1
+
+        for idx in range(len(nums) - 1, -1, -1):
+            if (idx + nums[idx]) >= last_position:
+                last_position = idx
+        
         return last_position == 0
