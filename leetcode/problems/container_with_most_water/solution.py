b0c167e25a0e9b1d81f110a497ca1359047f88e4
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-19 22:30:39 +0000

    Sync LeetCode submission - Container With Most Water (python3)

--- leetcode/problems/container_with_most_water/solution.py
+++ leetcode/problems/container_with_most_water/solution.py
@@ -1,13 +1,14 @@
 class Solution:
-    def maxArea(self, heights: List[int]) -> int:
-        left = 0
-        right = len(heights) - 1
-        maxWater = 0
-        while left < right < len(heights):
-            amount = min(heights[left], heights[right]) * (right - left)
-            maxWater = max(maxWater, amount)
-            if heights[left] <= heights[right]:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        max_volume = 0
+
+        while left < right:
+            volume = min(height[left], height[right]) * (right - left)
+            max_volume = max(max_volume, volume)
+            if height[right] > height[left]:
                 left += 1
             else:
                 right -= 1
-        return maxWater
+        
+        return max_volume
