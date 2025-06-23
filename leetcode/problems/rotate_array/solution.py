6c3ac6f53b697557021060b73ecf0094a136e2f3
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-23 17:23:37 +0000

    Sync LeetCode submission - Rotate Array (python3)

--- leetcode/problems/rotate_array/solution.py
+++ leetcode/problems/rotate_array/solution.py
@@ -5,12 +5,13 @@ class Solution:
         """
         n = len(nums)
         k %= n
-        self.reverse(nums, 0, n - 1)
-        self.reverse(nums, 0, k - 1)
-        self.reverse(nums, k, n - 1)
+
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
         
-    def reverse(self, nums, start, end):
-        while start < end:
-            nums[start], nums[end] = nums[end], nums[start]
-            start += 1
-            end -= 1
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
