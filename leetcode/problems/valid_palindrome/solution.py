02331e3355f94aba416806695dc66f28496b458c
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-24 22:07:39 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

--- leetcode/problems/valid_palindrome/solution.py
+++ leetcode/problems/valid_palindrome/solution.py
@@ -1,7 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        snew = ""
-        for ch in s:
-            if ch.isalnum():
-                snew += ch.lower()
-        return snew == snew[::-1]
+        left, right = 0, len(s) - 1
+        while left <= right:
+            if s[left].isalnum() and s[right].isalnum() and s[left].lower() != s[right].lower():
+                return False
+            elif not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            else:
+                left += 1
+                right -= 1
+        return True
