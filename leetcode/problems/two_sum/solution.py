79607ceb9f0a71a566216028afbaa1743c080a18
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-24 19:13:35 +0000

    Sync LeetCode submission - Two Sum (python3)

--- leetcode/problems/two_sum/solution.py
+++ leetcode/problems/two_sum/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hmap = {}
+        hmap = defaultdict(int)
+
         for idx, num in enumerate(nums):
             if num in hmap:
                 return [hmap[num], idx]
-            new_num = target - num
-            hmap[new_num] = idx
+            hmap[target - num] = idx
