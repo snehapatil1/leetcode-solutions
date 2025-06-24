8b7fc06a0eabf7f26ee4b7e3cd2a7dc6d8c51477
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-24 19:11:05 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- leetcode/problems/valid_anagram/solution.py
+++ leetcode/problems/valid_anagram/solution.py
@@ -1,3 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        s_counts = Counter(s)
+
+        for ch in t:
+            if ch not in s_counts or s_counts[ch] <= 0:
+                return False
+            s_counts[ch] -= 1
+        
+        return sum(s_counts.values()) == 0
