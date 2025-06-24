ea6d3a6bed8394bf0fda95f6f2f32ddb0e85e7bc
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-24 19:44:16 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- leetcode/problems/top_k_frequent_elements/solution.py
+++ leetcode/problems/top_k_frequent_elements/solution.py
@@ -1,31 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # counts = Counter(nums)
-        # heap = [(-counts[num], num) for num in counts]
-        # heapq.heapify(heap)
-        # output = []
-        # for i in range(k):
-        #     _, num = heapq.heappop(heap)
-        #     output.append(num)
-        # return output
-        # return heapq.nlargest(k, counts.keys(), key=counts.get)
-
         n = len(nums)
-        bucket = [[] for i in range(n + 1)]
-        output = []
+        buckets = [[] for _ in range(n + 1)]
         counts = Counter(nums)
+        output = []
+
+        for num in counts:
+            buckets[counts[num]].append(num)
         
-        for key in counts:
-            bucket[counts[key]].append(key)
-        
-        for idx in range(n, -1, -1):
+        for idx in range(len(buckets) - 1, -1, -1):
             if len(output) == k:
-                break
+                return output
             
-            if bucket[idx]:
-                cur = 0
-                while cur < len(bucket[idx]) and len(output) < k:
-                    output.append(bucket[idx][cur])
-                    cur += 1
-        
-        return output
+            for num in buckets[idx]:
+                if len(output) < k:
+                    output.append(num)
