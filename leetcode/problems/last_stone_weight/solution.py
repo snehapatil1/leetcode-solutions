50e5ef2e445ea70e4a6cb6b9ad5ed65904703d35
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-27 15:47:03 +0000

    Sync LeetCode submission - Last Stone Weight (python3)

--- leetcode/problems/last_stone_weight/solution.py
+++ leetcode/problems/last_stone_weight/solution.py
@@ -1,16 +1,14 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        heap = []
-        for stone in stones:
-            heapq.heappush(heap, -stone)
+        heap = [-weight for weight in stones]
+        heapq.heapify(heap)
         
-        while len(heap) > 0:
-            if len(heap) == 1:
-                return -heapq.heappop(heap)
-            stone2 = -heapq.heappop(heap)
-            stone1 = -heapq.heappop(heap)
-            if stone1 <= stone2:
-                stone2 -= stone1
-                heapq.heappush(heap, -stone2)
+        while len(heap) > 1:
+            y = - heapq.heappop(heap)
+            x = - heapq.heappop(heap)
+
+            if x < y:
+                y -= x
+                heapq.heappush(heap, -y)
         
-        return 0
+        return -heap[0] if len(heap) > 0 else 0
