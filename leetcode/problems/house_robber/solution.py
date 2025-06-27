c0d6af745e89060509d541a90a34bbd214fbed49
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-27 03:32:37 +0000

    Sync LeetCode submission - House Robber (python3)

--- leetcode/problems/house_robber/solution.py
+++ leetcode/problems/house_robber/solution.py
@@ -1,24 +1,5 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # ========== Bottom Up DP
-        
-        # n = len(nums)
-        # if n <= 2:
-        #     return max(nums)
-        
-        # dp = [0] * n
-        # dp[0] = nums[0]
-        # dp[1] = max(nums[0], nums[1])
-        # idx = 2
-
-        # while idx < n:
-        #     dp[idx] = max(dp[idx - 1], nums[idx] + dp[idx - 2])
-        #     idx += 1
-        
-        # return max(dp)
-
-        # ========== Bottom Up DP Space Optimized
-
         one_back, two_back = 0, 0
 
         for num in nums:
