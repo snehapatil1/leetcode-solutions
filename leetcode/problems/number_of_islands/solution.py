bc43e0b78f05692c1238456c3cdd05e9c92a5126
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-27 23:30:39 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- leetcode/problems/number_of_islands/solution.py
+++ leetcode/problems/number_of_islands/solution.py
@@ -2,19 +2,21 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         islands = 0
         rows, cols = len(grid), len(grid[0])
-        
-        def traverse(row, col):
-            if 0 <= row < rows and 0 <= col < cols and grid[row][col] == "1":
-                grid[row][col] = 'V'
 
-                traverse(row - 1, col)
-                traverse(row + 1, col)
-                traverse(row, col - 1)
-                traverse(row, col + 1)
+        def traverse(row, col):
+            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] != '1':
+                return False
+            
+            grid[row][col] = 'L'
+            
+            traverse(row - 1, col)
+            traverse(row + 1, col)
+            traverse(row, col - 1)
+            traverse(row, col + 1)
 
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == "1":
+                if grid[row][col] == '1':
                     islands += 1
                     traverse(row, col)
 
