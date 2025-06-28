da95d5d8629851af84f65c3fc41969a0c3f3c7c2
Sneha Patil <sneha.patil13sp@gmail.com>
2025-06-28 17:10:28 +0000

    Sync LeetCode submission - Max Area of Island (python3)

--- leetcode/problems/max_area_of_island/solution.py
+++ leetcode/problems/max_area_of_island/solution.py
@@ -1,22 +1,22 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        max_area = 0
         rows, cols = len(grid), len(grid[0])
-        maxArea = 0
         visited = set()
 
-        def dfs(row, col, area):
+        def traverse(row, col, area):
             if row < 0 or row >= rows or col < 0 or col >= cols or (row, col) in visited or grid[row][col] != 1:
                 return 0
             
             visited.add((row, col))
-            area = 1 + dfs(row + 1, col, area) + dfs(row - 1, col, area) + dfs(row, col + 1, area) + dfs(row, col - 1, area)
-
+            area = 1 + traverse(row - 1, col, area) + traverse(row + 1, col, area) + traverse(row, col - 1, area) + traverse(row, col + 1, area)
+            
             return area
-        
+
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 1:
-                    area = dfs(row, col, 0)
-                    maxArea = max(maxArea, area)
-        
-        return maxArea
+                    area = traverse(row, col, 0)
+                    max_area = max(max_area, area)
+
+        return max_area
