6b20834ca2b171fd1462f07e52e622903a6144fa
Sneha Patil <sneha.patil13sp@gmail.com>
2025-07-07 21:40:05 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- leetcode/problems/combination_sum/solution.py
+++ leetcode/problems/combination_sum/solution.py
@@ -1,17 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         combinations = []
-        def backtrack(first, curr, total):
+
+        def backtrack(i, curr, total):
             if total == 0:
                 combinations.append(curr[:])
                 return
+            
             if total < 0:
                 return
             
-            for i in range(first, len(candidates)):
-                curr.append(candidates[i])
-                backtrack(i, curr, total - candidates[i])
+            for idx in range(i, len(candidates)):
+                curr.append(candidates[idx])
+                backtrack(idx, curr, total - candidates[idx])
                 curr.pop()
         
         backtrack(0, [], target)
+
         return combinations
